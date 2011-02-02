svn2github/transmission-sequential

commit 50c6ba0713f8ad3df999889fcfc509fa4ac91232

(daemon) #3368:Edit trackers in transmission-remote
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@10907 f4695dd4-2c0a-0410-b89c-da849a56a58e


git-svn-id: https://github.com/ponychicken/transmission-sequential@9291 135cec4e-766b-f2b7-b973-ce57b7db7bff

trunk/daemon/remote.c
@@ -244,6 +244,8 @@ static tr_option opts[] =
     { 952, "no-seedratio",           "Let the current torrent(s) seed regardless of ratio", "SR", 0, NULL },
     { 953, "global-seedratio",       "All torrents, unless overridden by a per-torrent setting, should seed until a specific ratio", "gsr", 1, "ratio" },
     { 954, "no-global-seedratio",    "All torrents, unless overridden by a per-torrent setting, should seed regardless of ratio", "GSR", 0, NULL },
+    { 710, "tracker-add",            "Add a tracker to a torrent", "ta", 1, "<tracker>" },
+    { 712, "tracker-remove",         "Remove a tracker from a torrent", "tr", 1, "<trackerId>" },
     { 's', "start",                  "Start the current torrent(s)", "s",  0, NULL },
     { 'S', "stop",                   "Stop the current torrent(s)", "S",  0, NULL },
     { 't', "torrent",                "Set the current torrent(s)", "t",  1, "<torrent>" },
@@ -355,6 +357,7 @@ getOptMode( int val )
         case 993: /* no-trash-torrent */
             return MODE_SESSION_SET;
 
+        case 712: /* tracker-remove */
         case 950: /* seedratio */
         case 951: /* seedratio-default */
         case 952: /* no-seedratio */
@@ -370,6 +373,7 @@ getOptMode( int val )
         case 700: /* torrent priority-high */
         case 701: /* torrent priority-normal */
         case 702: /* torrent priority-low */
+        case 710: /* tracker-add */
         case 900: /* file priority-high */
         case 901: /* file priority-normal */
         case 902: /* file priority-low */
@@ -1934,6 +1938,12 @@ processArgs( const char * host, int port, int argc, const char ** argv )
 
             switch( c )
             {
+                case 712:
+                    {
+                        tr_benc * trackers = tr_bencDictAddDict( args, "trackerRemove", 1 );
+                        tr_bencDictAddInt( trackers, "id", atoi(optarg) );
+                        break;
+                    }
                 case 950: tr_bencDictAddReal( args, "seedRatioLimit", atof(optarg) );
                           tr_bencDictAddInt( args, "seedRatioMode", TR_RATIOLIMIT_SINGLE );
                           break;
@@ -1976,6 +1986,12 @@ processArgs( const char * host, int port, int argc, const char ** argv )
                           break;
                 case 702: tr_bencDictAddInt( args, "bandwidthPriority", -1 );
                           break;
+                case 710:
+                     {
+                         tr_benc * trackers = tr_bencDictAddDict( args, "trackerAdd", 1 );
+                         tr_bencDictAddStr( trackers, "announce", optarg );
+                         break;
+                     }
                 default:  assert( "unhandled value" && 0 );
                           break;
             }

trunk/daemon/transmission-remote.1
@@ -220,6 +220,10 @@ Let the current torrent(s) seed until a specific
 Let the current torrent(s) seed regardless of ratio
 .It Fl srd Fl -seedratio-default
 Let the current torrent(s) use the global seedratio settings
+.It Fl ta Fl -tracker-add Ar tracker
+Add a tracker to a torrent
+.It Fl ta Fl -tracker-remove Ar trackerId
+Remove a tracker from a torrent
 .It Fl s Fl -start
 Start the current torrent(s)
 .It Fl S Fl -stop

commit 0e8f5212a05a69acc76e934ede53f4448e9f6e27

(trunk daemon) #3775 "'-tall' no longer works for transmission-remote" -- fixed.
looks like this was broken in r10907 for #3368 in 2.00 when we added "-ta" as the short form of "--tracker-add". "-ta" seems to be conflicting with "-tall". This can be fixed by changing --tracker-add's short form.


git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@11809 f4695dd4-2c0a-0410-b89c-da849a56a58e


git-svn-id: https://github.com/ponychicken/transmission-sequential@10095 135cec4e-766b-f2b7-b973-ce57b7db7bff

trunk/daemon/remote.c
@@ -285,7 +285,7 @@ static tr_option opts[] =
     { 952, "no-seedratio",           "Let the current torrent(s) seed regardless of ratio", "SR", 0, NULL },
     { 953, "global-seedratio",       "All torrents, unless overridden by a per-torrent setting, should seed until a specific ratio", "gsr", 1, "ratio" },
     { 954, "no-global-seedratio",    "All torrents, unless overridden by a per-torrent setting, should seed regardless of ratio", "GSR", 0, NULL },
-    { 710, "tracker-add",            "Add a tracker to a torrent", "ta", 1, "<tracker>" },
+    { 710, "tracker-add",            "Add a tracker to a torrent", "td", 1, "<tracker>" },
     { 712, "tracker-remove",         "Remove a tracker from a torrent", "tr", 1, "<trackerId>" },
     { 's', "start",                  "Start the current torrent(s)", "s",  0, NULL },
     { 'S', "stop",                   "Stop the current torrent(s)", "S",  0, NULL },

trunk/daemon/transmission-remote.1
@@ -231,7 +231,7 @@ Let the current torrent(s) seed until a specific
 Let the current torrent(s) seed regardless of ratio
 .It Fl srd Fl -seedratio-default
 Let the current torrent(s) use the global seedratio settings
-.It Fl ta Fl -tracker-add Ar tracker
+.It Fl td Fl -tracker-add Ar tracker
 Add a tracker to a torrent
 .It Fl tr Fl -tracker-remove Ar trackerId
 Remove a tracker from a torrent