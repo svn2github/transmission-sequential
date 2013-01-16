svn2github/transmission-sequential

commit f0d5b7710e88126edace135e4dec3c7bf33fdb92

(cli) #5212 'cli fails when download directory doesn't exist' -- fixed.
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@13764 f4695dd4-2c0a-0410-b89c-da849a56a58e


git-svn-id: https://github.com/ponychicken/transmission-sequential@11939 135cec4e-766b-f2b7-b973-ce57b7db7bff

trunk/cli/cli.c
@@ -231,7 +231,6 @@ main (int argc, char ** argv)
   uint8_t     * fileContents;
   size_t        fileLength;
   const char  * str;
-  char          buf[TR_PATH_MAX];
 
   tr_formatter_mem_init (MEM_K, MEM_K_STR, MEM_M_STR, MEM_G_STR, MEM_T_STR);
   tr_formatter_size_init (DISK_K,DISK_K_STR, DISK_M_STR, DISK_G_STR, DISK_T_STR);
@@ -267,15 +266,16 @@ main (int argc, char ** argv)
 
   if (tr_variantDictFindStr (&settings, TR_KEY_download_dir, &str, NULL))
     {
-      str = tr_realpath (str, buf);
+      if (!tr_fileExists (str, NULL))
+        tr_mkdirp (str, 0700);
 
-      if (str != NULL)
+      if (tr_fileExists (str, NULL))
         {
           tr_variantDictAddStr (&settings, TR_KEY_download_dir, str);
         }
       else
         {
-          fprintf (stderr, "Download directory does not exist!\n");
+          fprintf (stderr, "Unable to create download directory \"%s\"!\n", str);
           return EXIT_FAILURE;
         }
     }

commit 0521f2c9d84fa2a6c4a216cf6929371b1ccc503b

(cli) #5212 'cli fails when download directory doesn't exist' -- fix 2.76 regression introduced in r13764 (trunk) / r13772 (branches/2.7x)
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@13793 f4695dd4-2c0a-0410-b89c-da849a56a58e


git-svn-id: https://github.com/ponychicken/transmission-sequential@11957 135cec4e-766b-f2b7-b973-ce57b7db7bff

trunk/cli/cli.c
@@ -267,16 +267,14 @@ main (int argc, char ** argv)
   if (tr_variantDictFindStr (&settings, TR_KEY_download_dir, &str, NULL))
     {
       if (!tr_fileExists (str, NULL))
-        tr_mkdirp (str, 0700);
+        {
+          tr_mkdirp (str, 0700);
 
-      if (tr_fileExists (str, NULL))
-        {
-          tr_variantDictAddStr (&settings, TR_KEY_download_dir, str);
-        }
-      else
-        {
-          fprintf (stderr, "Unable to create download directory \"%s\"!\n", str);
-          return EXIT_FAILURE;
+          if (!tr_fileExists (str, NULL))
+            {
+              fprintf (stderr, "Unable to create download directory \"%s\"!\n", str);
+              return EXIT_FAILURE;
+            }
         }
     }
 